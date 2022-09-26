95d33f60d81fdc986769cc20c3d84edd92bdd687
Senihil <takacsadri98@gmail.com>
2022-09-26 08:55:25 +0200

    Hello new world and added animal class

---
+++ src/main/java/hu/nye/progtech/Animal.java
@@ -0,0 +1,7 @@
+package hu.nye.progtech;
+
+public class Animal {
+    public void hello() {
+        System.out.println("Animal");
+    }
+}
--- src/main/java/hu/nye/progtech/Main.java
+++ src/main/java/hu/nye/progtech/Main.java
@@ -3,6 +3,6 @@ package hu.nye.progtech;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello new World!");
     }
 }
